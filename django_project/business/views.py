4fbcb9ca48c7947c9478970f33d7498b290edefa
Carlos Samaniego <cscarlossamaniego@gmail.com>
2019-08-18 15:05:54 -0400

    put in html so far, may tweek it to be more django-esque

--- django_project/business/views.py
+++ django_project/business/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Business Home</h1>')
+    return render(request, 'business/home.html')
