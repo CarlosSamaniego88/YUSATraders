d962ddafe8d0c5118bd2c733ca6407b8aed5473e
Carlos Samaniego <cscarlossamaniego@gmail.com>
2020-03-28 18:23:11 -0400

    added smooth scrolling and deleted unnecesary files

--- app.py
+++ app.py
@@ -3,12 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/home')
-def index():
     return render_template('home.html')
-
 
 if __name__ =='__main__':
     app.run(debug=True)
