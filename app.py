839805685525ad3de61bd05d8d303a4da07d43ac
Carlos Samaniego <cscarlossamaniego@gmail.com>
2020-03-28 16:55:42 -0400

    finished new website design in home.html, still need smooth scrolling

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/home')
+def index():
+    return render_template('home.html')
+
+
 if __name__ =='__main__':
     app.run(debug=True)
