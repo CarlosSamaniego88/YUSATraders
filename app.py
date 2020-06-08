4eac4f55f2a77447cf688ec97913997354a37a95
Carlos Samaniego <cscarlossamaniego@gmail.com>
2020-06-08 18:38:12 -0400

    removed contact us page

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ app.secret_key = 'development key'
 @app.route('/')
 def home():
     return render_template('home.html')
-    
-@app.route('/contact')
-def contact():
-  return render_template('contact.html')
 
 if __name__ =='__main__':
     app.run(debug=True)
